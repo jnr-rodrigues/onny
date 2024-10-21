7933d7ba8051939ff2b4d6a8e5d27bd0568d0953
Júnior Rodrigues <44007416+jnr-rodrigues@users.noreply.github.com>
2024-10-21 19:33:50 -0300

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -4,16 +4,15 @@ const moment = require("moment");
 moment.updateLocale("pt-br");
 
 module.exports = {
-  cooldown: 5, // Tempo de espera necessário entre o uso do comando (em segundos)
+  cooldown: 5,
   data: new SlashCommandBuilder()
-    .setName("ping") // Define o nome do comando
-    .setDescription("Pong! Saiba mais sobre sua latência."), // Descrição do comando
+    .setName("ping")
+    .setDescription("Pong! Saiba mais sobre sua latência."),
   async execute(interaction) {
-    await interaction.deferReply(); // Deferir a resposta inicial para evitar o tempo limite de resposta
-    // Calcula a latência do bot em relação ao servidor
+    await interaction.deferReply();
     const latency = Date.now() - interaction.createdTimestamp;
     await interaction.editReply({
-      content: `⏱️**P-Pong!** Sua latência é: ${Math.abs(latency)}ms.`, // Exibe a latência em uma resposta editada
+      content: `⏱️**P-Pong!** Sua latência é: ${Math.abs(latency)}ms.`,
     });
   },
 };
